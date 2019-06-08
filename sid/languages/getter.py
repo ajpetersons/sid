1679a443ef57feb13696903223038a8adb409350
Artūrs Jānis Pētersons <arturs.j.petersons@testdevlab.com>
2019-06-08 19:21:35 +0100

    Create base files for Matlab language implementation

--- sid/languages/getter.py
+++ sid/languages/getter.py
@@ -1,23 +1,29 @@
 from sid.languages.base.LanguageCleaner import LanguageCleaner
 from sid.languages.python3.Cleaner import Python3Cleaner
+from sid.languages.matlab.Cleaner import MatlabCleaner
 from sid.languages.errors import UnknownLanguageError
-# from sid.languages.matlab.Cleaner import MatlabCleaner
 
 
 def get_language_parser(name):
-    """TODO:
+    """Function retrieves an uninitialized parser for each language. Each parser 
+        should extend LanguageCleaner class. If text needs to be mapped 1:1 from 
+        parser, with indices computed, LanguageCleaner itself has base 
+        implementation that can provide such functionality without knowing 
+        anything about source code syntax.
     
     :param name: Name of the language to use
     :type name: str
+    :raises UnknownLanguageError: Exception is raised when an unknown language 
+        is passed to function, letting user know that this language is not 
+        supported
     :return: Class instance of language type initialized for processing
     :rtype: function
     """
     
     languages = {
-        # FIXME: refactor for correct indices
         "none": LanguageCleaner,
         "python3": Python3Cleaner,
-        # "matlab": MatlabCleaner
+        "matlab": MatlabCleaner
     }
 
     if name in languages:
---
+++ sid/languages/matlab/Cleaner.py
@@ -0,0 +1,45 @@
+from antlr4 import *
+
+from sid.languages.base.LanguageCleaner import LanguageCleaner
+from sid.languages.matlab.matlabLexer import matlabLexer
+from sid.languages.matlab.matlabParser import matlabParser
+from sid.languages.matlab.Walker import SIDMatlabWalker
+
+
+class MatlabCleaner(LanguageCleaner):
+    """Class facilitates Matlab language cleaner. This is a useful, reusable 
+        wrapper around ANTLR commands needed to parse a program and to turn it 
+        into tokens as expected by similarity detector.
+    """
+
+    def __init__(self):
+        """Method initializes a new instance of Matlab language cleaner
+        """
+        super().__init__()
+        self.name = "matlab"
+
+
+    def clean(self, file):
+        """Method takes an input file name, reads this file an parses it with 
+            ANTLR. This method wraps basic ANTLR actions that would be necessary 
+            in order to simplify language processing.
+        
+        :param file: Path to the file, which contains code to be processed
+        :type file: str
+        :return: Sequence of processed file contents by obfuscating code in a 
+            way to reveal only the underlying structure. Also returns locations 
+            of the characters in the original files as a list
+        :rtype: tuple of str and list of dict
+        """
+        contents = FileStream(file)
+        lexer = matlabLexer(contents)
+        stream = CommonTokenStream(lexer)
+        parser = matlabParser(stream)
+
+        tree = parser.file_input()
+        listener = SIDMatlabWalker()
+
+        walker = ParseTreeWalker()
+        walker.walk(listener, tree)
+
+        return listener.str_symbols(), listener.tokens
