498df3b9d3944b80a51bca8dd6ba3b6402f17c70
Artūrs Jānis Pētersons <arturs.j.petersons@testdevlab.com>
2019-06-10 20:08:23 +0100

    Add matlab walker class

---
+++ sid/languages/matlab2/Cleaner.py
@@ -0,0 +1,47 @@
+from antlr4 import *
+
+from sid.languages.base.LanguageCleaner import LanguageCleaner
+from sid.languages.matlab2.MATLABLexer import MATLABLexer
+from sid.languages.matlab2.MATLABParser import MATLABParser
+from sid.languages.matlab2.Walker import SIDMatlabWalker
+
+
+class MatlabCleaner(LanguageCleaner):
+    """Class facilitates Matlab language cleaner. This is a useful, reusable 
+        wrapper around ANTLR commands needed to parse a program and to turn it 
+        into tokens as expected by similarity detector.
+    """
+
+    def __init__(self):
+        """Method initializes a new instance of Matlab language cleaner
+        """
+        super().__init__()
+        self.name = "matlab"
+
+
+    def clean(self, file):
+        """Method takes an input file name, reads this file an parses it with 
+            ANTLR. This method wraps basic ANTLR actions that would be necessary 
+            in order to simplify language processing.
+        
+        :param file: Path to the file, which contains code to be processed
+        :type file: str
+        :return: Sequence of processed file contents by obfuscating code in a 
+            way to reveal only the underlying structure. Also returns locations 
+            of the characters in the original files as a list
+        :rtype: tuple of str and list of dict
+        """
+        contents = FileStream(file)
+        lexer = MATLABLexer(contents)
+        stream = CommonTokenStream(lexer)
+        parser = MATLABParser(stream)
+
+        tree = parser.fileDecl()
+        listener = SIDMatlabWalker()
+
+        walker = ParseTreeWalker()
+        walker.walk(listener, tree)
+
+        print(listener)
+
+        return listener.str_symbols(), listener.tokens
